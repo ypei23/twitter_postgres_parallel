cba2d3106835f0072c27e002c6fb5873cb9ad01e
Yuechen.Pei <Yuechen.Pei@cmc.edu>
2024-04-08 10:54:08 -0700

    third

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,7 +6,7 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-	unzip -p test-data.zip | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:1352" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+	unzip -p test-data.zip | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:54321" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 
@@ -14,7 +14,7 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-	python3 -u load_tweets.py --db postgresql://postgres:pass@localhost:2351 --inputs $file
+	python3 -u load_tweets.py --db postgresql://postgres:pass@localhost:54322 --inputs $file
 done
 
 
@@ -22,5 +22,5 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:54323/ --inputs $file
 done
